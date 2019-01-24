76fa825d360f30bc39713377e1cfa74db860c07b
trishaghosh <tghosh@purdue.edu>
2019-01-24 09:18:30 -0500

    code for project

---
+++ Flixx/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flixx
+//
+//  Created by Trisha Ghosh  on 1/23/19.
+//  Copyright © 2019 Trisha Ghosh . All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
